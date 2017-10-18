c7c8b22f0f1911ee33a626a70c06bca82ccdfd1b
DionathaRodrigues <dionatha.stricker@gmail.com>
2017-10-18 00:25:08 -0200

    começando adicionar funções do firebase

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = var config = {
+        apiKey: "AIzaSyDpSjdndm7kIp6MrN0c9gXJk52RA-L3Vg8",
+        authDomain: "fixcar-teste.firebaseapp.com",
+        databaseURL: "https://fixcar-teste.firebaseio.com",
+        projectId: "fixcar-teste",
+        storageBucket: "",
+        messagingSenderId: "674528631700"
+    };
+  
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,20 +1,27 @@
 import { CadastroVeiculosPage } from './../cadastro-veiculos/cadastro-veiculos';
+import { CadastroClientesPage } from './../cadastro-clientes/cadastro-clientes';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import {User} from "../../models/user";
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
+  user= {} as User;
+
   constructor(public navCtrl: NavController) {
 
   }
 
-  onEntrar(): void{
+  onLogin(): void{
     this.navCtrl.push(CadastroVeiculosPage);
 
   }
 
+  onRegister(): void{
+    this.navCtrl.push(CadastroClientesPage);
+
+  }
 }
