b1660735f8afb6dc6ab75cb67064f2918f590b1f
Dionatha Rodrigues <dionatharodrigues@MacBook-Pro-de-Dionatha.local>
2017-10-18 22:16:01 -0200

    teste

--- src/pages/cadastro-veiculos/cadastro-veiculos.ts
+++ src/pages/cadastro-veiculos/cadastro-veiculos.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+import {AngularFireAuth} from 'angularfire2/auth';
 
 /**
  * Generated class for the CadastroVeiculosPage page.
@@ -15,13 +16,28 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CadastroVeiculosPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private afAuth: AngularFireAuth,
+    public navCtrl: NavController, public navParams: NavParams) {
   }
-
-  
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CadastroVeiculosPage');
   }
 
+  /*ionViewWillLoad() {
+    this.afAuth.authState.subscribe(data => {
+      if (data && data.email && data.uid){
+      this.toast.create({
+        message: 'Bem vindo ao FixCar, ${data.email}',
+        duration: 3000
+      }).present();
+    }
+    else {
+      this.toast.create({
+        message: 'Usuário ou Senha inválidos, por favor verifique.',
+        duration: 3000
+    })
+  }
+}
+)}*/
 }
